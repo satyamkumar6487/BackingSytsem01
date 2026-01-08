c3688098ddde6e661f34eb6dc00b5ab314ff6e25
satyamkumar6487 <jsatyamkumar597@gmail.com>
2026-01-08 22:49:36 +0530

    add all the method

--- src/app/Main.java
+++ src/app/Main.java
@@ -26,12 +26,68 @@ while (true) {
             """);
 System.out.println("CHOSE");
 
+
     String Chose = sc.nextLine().trim();
     System.out.println(Chose);
 
+    switch (Chose) {
+        case "1" -> openAccount(sc);
+        case "2" -> deposit(sc);
+        case "3" -> withdraw(sc);
+        case "4" -> transfer(sc);
+        case "5" -> statements(sc);
+        case "6" -> listAccount(sc);
+        case "7" -> searchAccount(sc);
+        case "0" -> running = false;
 
-}
 
 
     }
 }
+
+
+    }
+    private static void openAccount(Scanner sc) {
+
+        System.out.println(" Enter Name ");
+        String name = sc.nextLine().trim();
+
+        System.out.println("Enter Email ");
+        String email = sc.nextLine().trim();
+
+        System.out.println("Account Type / SAVING / CURRENT");
+        String type = sc.nextLine().trim();
+
+
+        System .out.println("Initial Depostit");
+        String amountstr = sc. nextLine().trim();
+
+        Double initial = Double.valueOf(amountstr);
+    }
+
+        private static void deposit(Scanner sc) {
+    }
+
+    private static void withdraw(Scanner sc) {
+
+    }
+
+    private static void transfer (Scanner sc) {
+
+    }
+
+    private static void statements(Scanner sc) {
+
+    }
+
+    private static void listAccount(Scanner sc) {
+
+    }
+
+    private static void searchAccount(Scanner sc) {
+
+    }
+
+
+}
+
